f3f46b436a0745bf04e1a140e3d4ea108c27bebc
Santiago Palacios <santipalacios2002@gmail.com>
2021-06-27 22:29:56 -0400

    added login route for api/users/login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -27,49 +27,42 @@ router.post('/', async (req, res) => {
   }
 });
 
-// // Login
-// router.post('/login', async (req, res) => {
-//   try {
-//     const dbUserData = await User.findOne({
-//       where: {
-//         email: req.body.email,
-//       },
-//     });
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    //find user with email
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    //if it is not found return message
+    if (!dbUserData) {
+      res.status(400).json({ message: 'Incorrect email. Please try again!' });
+      return;
+    }
+    //if use found but password is not valid then through message
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password. Please try again!' });
+      return;
+    }
 
-//     if (!dbUserData) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password. Please try again!' });
-//       return;
-//     }
-
-//     const validPassword = await dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password. Please try again!' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       req.session.loggedIn = true;
-
-//       res
-//         .status(200)
-//         .json({ user: dbUserData, message: 'You are now logged in!' });
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json({ user: [dbUserData.username, dbUserData.email], message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // // Logout
 // router.post('/logout', (req, res) => {
 //   if (req.session.loggedIn) {
 //     req.session.destroy(() => {
-//       res.status(204).end();
+//       res.status(204).json({message: 'You successfully logged out'}).end();
 //     });
 //   } else {
 //     res.status(404).end();
