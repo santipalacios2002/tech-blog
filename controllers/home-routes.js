a6f54ffc81a1c38d0cce8a5ffff05d166f1d948a
Santiago Palacios <santipalacios2002@gmail.com>
2021-06-29 16:41:52 -0400

    added main layout and login view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,17 @@ const path = require("path");
 router.get("/", async (req, res) => {
   try {
     console.log("went to route");
-    res.status(200).json({ message: "went to root" });
+    res.render('all');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    console.log("went to login route");
+    res.render('login');
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
